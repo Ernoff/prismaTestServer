a31528f681b3dcc0bd14dab69569c2730236b4e8
Ernoff <ernest.offiong@gmail.com>
2018-01-24 17:49:41 +0100

    feat: server built up

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+  Query: {
+    feed(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: true } }, info);
+    },
+    state(parent, args, ctx, info) {
+      return ctx.db.query.states(info);
+    },
+    drafts(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: false } }, info);
+    },
+    post(parent, { id }, ctx, info) {
+      return ctx.db.query.post({ where: { id } }, info);
+    }
+  },
+  Mutation: {
+    createDraft(parent, { title, text }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        {
+          data: {
+            title,
+            text,
+            isPublished: false
+          }
+        },
+        info
+      );
+    },
+    createState(parent, { title, code }, ctx, info) {
+      return ctx.db.mutation.createState(
+        {
+          data: {
+            title,
+            code,
+          }
+        },
+        info
+      );
+    },
+    deletePost(parent, { id }, ctx, info) {
+      return ctx.db.mutation.deletePost({ where: { id } }, info);
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { isPublished: true }
+        },
+        info
+      );
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/public-sandpanther-975/my-app/dev', // the endpoint of the Prisma DB service
+      secret: 'mysecret123', // specified in database/prisma.yml
+      debug: true, // log all GraphQL queryies & mutations
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
